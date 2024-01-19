71f5fec16efc4436a95d6f7aeb17fab8dcd384b3
George Hamann <george.hamann@gmail.com>
2024-01-19 20:14:05 -0300

    Parte 7 do tutorial

--- src/Aula01/Senai.Asp.Net.Core.Mvc.MvcMovie/Controllers/MoviesController.cs
+++ src/Aula01/Senai.Asp.Net.Core.Mvc.MvcMovie/Controllers/MoviesController.cs
@@ -20,10 +20,56 @@ namespace Senai.Asp.Net.Core.Mvc.MvcMovie.Controllers
         }
 
         // GET: Movies
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(string movieGenre, string searchString)
         {
-            return View(await _context.Movie.ToListAsync());
+            if (_context.Movie == null)
+            {
+                return Problem("Entity set 'MvcMovieContext.Movie'  is null.");
+            }
+
+            // Use LINQ to get list of genres.
+            IQueryable<string> genreQuery = from m in _context.Movie
+                                            orderby m.Genre
+                                            select m.Genre;
+            var movies = from m in _context.Movie
+                         select m;
+
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                movies = movies.Where(s => s.Title!.Contains(searchString));
+            }
+
+            if (!string.IsNullOrEmpty(movieGenre))
+            {
+                movies = movies.Where(x => x.Genre == movieGenre);
+            }
+
+            var movieGenreVM = new MovieGenreViewModel
+            {
+                Genres = new SelectList(await genreQuery.Distinct().ToListAsync()),
+                Movies = await movies.ToListAsync()
+            };
+
+            return View(movieGenreVM);
         }
+
+        /*public async Task<IActionResult> Index(string searchString)
+        {
+            if (_context.Movie == null)
+            {
+                return Problem("Entity set 'SenaiAspNetCoreMvcMvcMovieContext.Movie'  is null.");
+            }
+
+            var movies = from m in _context.Movie
+                         select m;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                movies = movies.Where(s => s.Title!.Contains(searchString));
+            }
+
+            return View(await movies.ToListAsync());
+        }*/
 
         // GET: Movies/Details/5
         public async Task<IActionResult> Details(int? id)
@@ -153,5 +199,7 @@ namespace Senai.Asp.Net.Core.Mvc.MvcMovie.Controllers
         {
             return _context.Movie.Any(e => e.Id == id);
         }
+
+
     }
 }
---
+++ src/Aula01/Senai.Asp.Net.Core.Mvc.MvcMovie/Models/MovieGenreViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Senai.Asp.Net.Core.Mvc.MvcMovie.Models;
+public class MovieGenreViewModel
+{
+    public List<Movie>? Movies { get; set; }
+    public SelectList? Genres { get; set; }
+    public string? MovieGenre { get; set; }
+    public string? SearchString { get; set; }
+}
