7655a27461aa8e1006cd6b949eed0d732965c647
George Hamann <george.hamann@gmail.com>
2024-02-01 21:48:42 -0300

    Começo de exemplo do ponto

---
+++ Senai.Asp.Net.Core.Mvc/Senai.Asp.Net.Core.Mvc.Ponto/Controllers/PontoController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Senai.Asp.Net.Core.Mvc.Ponto.Models;
+
+namespace Senai.Asp.Net.Core.Mvc.Ponto.Controllers
+{
+    public class PontoController : Controller
+    {
+        public static Funcionario Funcionario { get; set; }
+        public PontoController() 
+        {
+            if(Funcionario == null)
+            {
+                Funcionario = new Funcionario
+                {
+                    Id = 1,
+                    Nome = "João da Silva",
+                    Idade = 30,
+                    CPF = "123.456.789-01",
+                    RG = "987654321",
+                    Telefone = "(11) 1234-5678",
+                    Email = "joao.silva@example.com"
+                };
+            }
+        }
+        public IActionResult Index()
+        {
+            return View(Funcionario);
+        }
+    }
+}
---
+++ Senai.Asp.Net.Core.Mvc/Senai.Asp.Net.Core.Mvc.Ponto/Models/Ponto.cs
@@ -0,0 +1,67 @@
+﻿namespace Senai.Asp.Net.Core.Mvc.Ponto.Models
+{
+    public class Ponto
+    {       
+        public List<Registro> Registros { get; set; }
+    }
+    public class Registro
+    {
+        public DateTime Entrada { get; set; }
+        public DateTime Saida { get; set; }
+        public TimeSpan Tempo { get; set; }
+        public TipoDeRegistro Tipo { get; set; }
+    }
+    public enum TipoDeRegistro
+    {
+        Normal = 0,
+        Noturno = 1,
+        Intervalo = 2,
+        Desconhecido = 3,
+        HoraExtra = 4
+    }
+    public class Funcionario
+    {
+        public Funcionario()
+        {
+            PerfilDeTrabalho = PerfilDeTrabalho.PerfilDeTrabalhoPadrao();
+            ListaDePontos = new List<Ponto>();
+        }
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public string CPF { get; set; }
+        public string RG { get; set; }
+        public string Telefone { get; set; }
+        public string Email { get; set; }
+        public PerfilDeTrabalho PerfilDeTrabalho { get; set; }
+        public List<Ponto> ListaDePontos { get; set; }
+    }
+    public class PerfilDeTrabalho
+    {
+        private static PerfilDeTrabalho Padrao { get; set; }
+
+        public static PerfilDeTrabalho PerfilDeTrabalhoPadrao()
+        {
+            if (Padrao == null)
+            {
+                Padrao = new PerfilDeTrabalho()
+                {
+                    Fixo = true,
+                    HoraFim = new DateTime(2024, 01, 01, 18, 00, 00),
+                    HoraInicio = new DateTime(2024, 01, 01, 08, 00, 00),
+                    InicioIntervalo = new DateTime(2024, 01, 01, 12, 00, 00),
+                    TempoDeIntervalo = TimeSpan.FromHours(2),
+                    Tolerancia = TimeSpan.FromMinutes(10)
+                };
+            }
+            return Padrao;
+        }
+
+        public bool Fixo { get; set; }
+        public DateTime HoraInicio { get; set; }
+        public DateTime HoraFim { get; set; }
+        public DateTime InicioIntervalo { get; set; }
+        public TimeSpan TempoDeIntervalo { get; set; }
+        public TimeSpan Tolerancia { get; set; }
+    }
+}
